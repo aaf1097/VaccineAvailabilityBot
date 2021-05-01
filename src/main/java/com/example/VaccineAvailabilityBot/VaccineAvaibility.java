b9e400dd6176712ccb36bb553c3f5fb9c12ffcda
Aaron Fernandes <aaronfernandes345@gmail.com>
2021-05-02 00:32:00 +0530

    Updated date code

--- src/main/java/com/example/VaccineAvailabilityBot/VaccineAvaibility.java
+++ src/main/java/com/example/VaccineAvailabilityBot/VaccineAvaibility.java
@@ -2,16 +2,10 @@ package com.example.VaccineAvailabilityBot;
 
 public class VaccineAvaibility {
 
-	private final long id;
 	private final String content;
 
-	public VaccineAvaibility(long id, String content) {
-		this.id = id;
+	public VaccineAvaibility( String content) {
 		this.content = content;
-	}
-
-	public long getId() {
-		return id;
 	}
 
 	public String getContent() {
